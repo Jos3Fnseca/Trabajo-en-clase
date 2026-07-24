becefc9297f8a8efd9b7e796d3ecbee1416c541a
José Pablo Fonseca Fernández <josepafonseca@hotmail.com>
2026-07-23 20:38:17 -0600

    Semana 11

--- Desarrollo Web/Practicas/src/app/Service/varios.service.ts
+++ Desarrollo Web/Practicas/src/app/Service/varios.service.ts
@@ -1,14 +1,18 @@
 import { effect, Injectable, signal } from "@angular/core";
 import { Character } from "../interface/character.interface";
 
+const loadFromLocalStorage = (): Character[]=>{
+    const characters = localStorage.getItem('Personajes');
+    return characters ? JSON.parse(characters): [];
+}
+
 @Injectable({providedIn: "root"})
 export class VariosService{
-        characters = signal<Character[]>([
-        {id:1, name:'Goku', power:9001},
-        {id:2, name:'Vegeta',power:8000},
-
-    ]);
+        characters = signal<Character[]>(loadFromLocalStorage());
     saveToLocalStorage = effect(()=>{
+        //()=> significa autoejecute una función anonima
+        //efecto=lo que quiero que se ejectute cuando pasa algun efecto
+        //en este caso, cuando se agrega un personaje se guarda en el local storage
         console.log(`Conteo de Personajes ${this.characters().length}`);
         localStorage.setItem('Personajes',JSON.stringify(this.characters()));//para guardar en local stroge del navegador
     });
